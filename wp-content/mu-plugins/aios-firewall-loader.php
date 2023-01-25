80226d8134d134b7f597ef39c9aa6f28d3ebdef0
Andrew McCallum <andrew.m@affinity.ad>
2023-01-25 11:56:50 +1100

    add base plugins

---
+++ wp-content/mu-plugins/aios-firewall-loader.php
@@ -0,0 +1,6 @@
+<?php
+// Begin AIOWPSEC Firewall
+if (file_exists('/Users/andrewm/Development/playground/wordpress/app/public/aios-bootstrap.php')) {
+	include_once('/Users/andrewm/Development/playground/wordpress/app/public/aios-bootstrap.php');
+}
+// End AIOWPSEC Firewall
