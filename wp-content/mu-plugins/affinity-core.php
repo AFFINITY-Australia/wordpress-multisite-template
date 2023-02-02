6021be9ad5ef1f406d384b36ce293676db0b4a3e
Andrew McCallum <andrew.m@affinity.ad>
2023-02-02 16:45:40 +1100

    add wp-sentry

---
+++ wp-content/mu-plugins/affinity-core.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: AFFINITY Core Plugin
+Description: AFFINITY Core Functions
+Author: AFFINITY, Andrew McCallum
+Author URI: https://affinity.ad
+Version: 1.0.0
+Requires at least: 4.1
+Tested up to: 6.0
+Requires PHP: 7.0
+Domain Path: languages
+Text Domain: affinity-core
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// don't load directly
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+
+define( 'WP_SENTRY_PHP_DSN', 'https://2f8612849d694f5cb5e21b7d0a51b1d4@o4504601309085696.ingest.sentry.io/4504608683130880' );
